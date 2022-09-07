1ef343af402aaa148ace0a1e57f299d8a2fe7b90
HasiThish <86554418+HasiThish@users.noreply.github.com>
2022-09-08 00:28:52 +0530

    added navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,8 +5,8 @@ const Header = () => {
 
     //views
     const showNavigation = () => (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <Link to="#" className="navbar-brand" href="#">Navbar</Link>
+        <nav style={{margin:'5px', paddingLeft:'20px'}} className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <Link to="#" className="navbar-brand" >Grocery Store</Link>
             <button 
                 className="navbar-toggler" 
                 type="button" 
@@ -21,31 +21,20 @@ const Header = () => {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
+                        <Link to="#" className="nav-link">Products </Link>
+                    </li>
+                    <li className="nav-item">
                         <Link to="#" className="nav-link">Sign Up </Link>
                     </li>
                     <li className="nav-item">
                         <Link to="#" className="nav-link">Sign In</Link>
                     </li>
-                    <li className="nav-item dropdown">
-                        <Link to="#" className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
-                        </Link>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <Link to="#" className="dropdown-item" href="#">Action</Link>
-                        <Link to="#" className="dropdown-item" href="#">Another action</Link>
-                        <div clclassNameass="dropdown-divider"></div>
-                        <Link to="#" className="dropdown-item" href="#">Something else here</Link>
-                        </div>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="#" className="nav-link disabled" href="#">Disabled</Link>
-                    </li>
+                    
+                    
                 </ul>
-                <form className="form-inline my-2 my-lg-0">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
-            </div>
+                </div>
+                
+            
         </nav>
     );
 
