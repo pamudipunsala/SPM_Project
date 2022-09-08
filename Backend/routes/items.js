3ea5629b0dad33aca46bbb50209afa47f7fc4058
Pamudi Punsala <88242530+pamudipunsala@users.noreply.github.com>
2022-09-08 12:24:42 +0530

    deleteRoute

--- Backend/routes/items.js
+++ Backend/routes/items.js
@@ -46,4 +46,31 @@ router.get("/items/:id", (req,res) => {
     });
 });
 
+router.put('/items/update/:id',(req,res)=>{
+    Items.findByIdAndUpdate(
+        req.params.id,{
+            $set:req.body
+        },
+        (err)=>{
+            if(err){
+                return res.status(400).json({error:err});
+            }
+            return res.status(200).json({
+                success:"Updated Successfully"
+            });
+        }
+    );
+}); 
+
+router.delete('/items/delete/:id',(req,res)=>{
+    Items.findByIdAndRemove(req.params.id).exec((err,deleteItem)=>{
+        if(err) return res.status(400).json({
+            message:"Couldn't Delete Details",err
+        });
+        return res.json({
+            message:"Details Deleted successfully", deleteItem
+        });
+    });
+});
+
 module.exports = router;
