11d2aece6cdcb0992eed296d3ad78d725bdd9145
Hansaka Eranda <87437151+HansakaEranda@users.noreply.github.com>
2022-10-21 18:12:27 +0530

    add signup form

--- frontend/src/Pages/Signup.js
+++ frontend/src/Pages/Signup.js
@@ -1,57 +1,34 @@
-import React, {useState} from 'react';
-import "../index.css"
-import axios from "axios"
-import { useNavigate } from "react-router-dom"
+import React from 'react';
+import '../index.css';
 
 const Signup = () => {
-
-    const history = useNavigate()
-
-    const [ users, setUsers] = useState({
-        uname:"",
-        regNo:"",
-        uemail:"",
-        userName:"",
-        upwd:""
-    })
-
-    const handleChange = e => {
-        const { name, value } = e.target
-        setUsers({
-            ...users,
-            [name]: value
-        })
-    }
-
-    const register = () => {
-        const { uname, regNo, uemail, userName, upwd } = users
-        if( uname && regNo && uemail && userName && upwd){
-            axios.post("http://localhost:5000/register", users)
-            .then( res => {
-                alert(res.data.message)
-                history.push("/login")
-            })
-        } else {
-            alert("invlid input")
-        }
-    }
-
-    return (
+    const showSignupForm = () => (
+        <form>
         <div className="container">
         <div className="register">
-            {console.log("Users", users)}
-            <h1>SignUp</h1>
-            <input type="text" name="uname" value={users.uname} placeholder="Enter Your Name" onChange={ handleChange }></input>
-            <input type="text" name="regNo" value={users.regNo} placeholder="Enter Your Register Number" onChange={ handleChange }></input>
-            <input type="text" name="uemail" value={users.uemail} placeholder="Enter Your Email" onChange={ handleChange }></input>
-            <input type="text" name="userName" value={users.userName} placeholder="Enter UserName" onChange={ handleChange }></input>
-            <input type="password" name="upwd" value={users.upwd} placeholder="Enter Password" onChange={ handleChange }></input>
-            <div className="button" onClick={register} >SignUp</div>
-            <div>or</div>
-            <div className="button" onClick={() => history.push("/login")}>Login</div>
+            
+            <h1>Register</h1>
+            <input type="text" name="stname" value="" placeholder="Username" ></input>
+            <input type="text" name="stemail" value="" placeholder="Email address" ></input>
+            <input type="text" name="stuserName" value="" placeholder="Create password" ></input>
+            <input type="password" name="stpwd" value="" placeholder="Confirm password" ></input>
+            <div className="button" >Signup</div>
+            
         </div>
+        </div>
+        </form>
+    );
+
+    return (
+        <div className='signup-container'>
+            <div className='row px-3 vh-100'>
+                <div className='col-md-5 mx-auto align-self-center'>
+                    {showSignupForm()}
+                </div>
+            </div>         
         </div>
     )
+        
 };
 
 export default Signup;
