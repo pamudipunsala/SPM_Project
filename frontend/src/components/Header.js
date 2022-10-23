700d5d85609cf2446002a32c1ef473e277a64c1c
Kusal Fernando <it20264666@my.sliit.lk>
2022-10-23 09:12:34 +0530

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -40,7 +40,15 @@ const Header = () => {
                     <li className="nav-item">
                         <a href={'/paylist'} className="nav-link">Payment List</a>
                     </li>
-                    
+                    <li className="nav-item">
+                        <a href={'/viewteam'} className="nav-link">Delivery team</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href={'/addreq'} className="nav-link">Delivery</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href={'/viewreq'} className="nav-link">Delivery Requests</a>
+                    </li>
                     
 
 
